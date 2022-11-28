beeec1d05401975bd7d1e722a0618eb9f991ab6b
grafondato <gracielafondato@gmail.com>
2022-11-27 21:24:30 -0300

    ejercitacion 10

---
+++ javascrip/scrip.js
@@ -0,0 +1,33 @@
+
+let nombre = "programacion" + "facil";
+let saludo = "le doy la bienvenida a ";
+let pregunta = "¿listo para comenzar?";
+let frasecompleta = saludo + nombre + pregunta;
+
+console.log(frasecompleta);
+
+let frase = "javascript" + "es un lenguaje" + "esencial para crear" + "sitios web poderosos";
+
+console.log(frase);
+
+ let numerostring1 = "10";
+ let numerostring2 = "20";
+ let numerostring3 = "30";
+
+ let concatenanumeros = numerostring1 + numerostring2 + numerostring3;
+
+ console.log(concatenanumeros);
+
+ let name = prompt("cual es tu nombre?");
+ let age = prompt("cual es tu edad?");
+
+ console.warn(name + age);
+
+ let edad;
+ edad = prompt("introduce tu edad");
+
+ if (edad >= 18){
+    alert("puedes entrar, eres mayor de edad");
+ } else {
+    alert("no puedes entrar, eres menor de edad");
+ }
